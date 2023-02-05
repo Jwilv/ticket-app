1c2c7969e7647fd13fd381053b65d7bd2aa96f70
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-02-05 15:57:31 -0300

    se agrego layout y se creo la page Turnos

---
+++ src/TicketApp.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { RouterPage } from "./pages/RouterPage"
+
+function TicketApp() {
+
+  return (
+    <div >
+      <RouterPage />
+    </div>
+  )
+}
+
+export default TicketApp
