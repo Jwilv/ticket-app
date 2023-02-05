3da8df5ca10150a4c082fbfc01f23825ab4a9c8c
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-02-05 18:46:50 -0300

    se crea el formulario de Ingresaar

--- src/pages/Ingresar.jsx
+++ src/pages/Ingresar.jsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import React from 'react';
+import { Button, Checkbox, Form, Input } from 'antd';
 
 export default function Ingresar() {
+
+    const onFinish = (values) => {
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+
     return (
-        <div>Ingresar</div>
+        <Form
+            name="basic"
+            labelCol={{ span: 4 }}
+            wrapperCol={{ span: 20 }}
+            style={{ maxWidth: 600 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete={'off'}
+        >
+            <Form.Item
+                label="Username"
+                name="username"
+                rules={[{ required: true, message: 'Please input your username!' }]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                label="Password"
+                name="password"
+                rules={[{ required: true, message: 'Please input your password!' }]}
+            >
+                <Input.Password />
+            </Form.Item>
+
+            <Form.Item wrapperCol={{ offset: 4, span: 16 }}>
+                <Button type="primary" htmlType="submit" shape='round'>
+                    Ingresar
+                </Button>
+            </Form.Item>
+        </Form>
     )
 }
