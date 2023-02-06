a3369ff8c991da91053155bbb77a44c6e8e73dcb
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-02-06 16:02:25 -0300

    page Turnos

--- src/pages/Turnos.jsx
+++ src/pages/Turnos.jsx
@@ -1,7 +1,95 @@
 import React from 'react'
+import { Card, Col, Divider, List, Row, Tag, Typography } from 'antd'
+import Item from 'antd/es/list/Item';
 
 export const Turnos = () => {
-  return (
-    <div>Turnos</div>
-  )
+
+    const { Title, Text } = Typography
+
+    const data = [
+        {
+            ticketNo: 33,
+            escritorio: 3,
+            agente: 'Fernando Herrera'
+        },
+        {
+            ticketNo: 34,
+            escritorio: 4,
+            agente: 'Melissa Flores'
+        },
+        {
+            ticketNo: 35,
+            escritorio: 5,
+            agente: 'Carlos Castro'
+        },
+        {
+            ticketNo: 36,
+            escritorio: 3,
+            agente: 'Fernando Herrera'
+        },
+        {
+            ticketNo: 37,
+            escritorio: 3,
+            agente: 'Fernando Herrera'
+        },
+        {
+            ticketNo: 38,
+            escritorio: 2,
+            agente: 'Melissa Flores'
+        },
+        {
+            ticketNo: 39,
+            escritorio: 5,
+            agente: 'Carlos Castro'
+        },
+    ];
+
+    return (
+        <>
+            <Title level={1}>Atendiendo al cliente</Title>
+            <Row>
+                <Col span={12}>
+                    <List
+                        dataSource={data.slice(0, 3)}
+                        renderItem={(item) => (
+                            <List key={item.agente}>
+                                <Item style={{ fontSize: 23 }}>
+                                    <Card
+                                        style={{ width: 300, marginTop: 16 }}
+                                        actions={[
+                                            <Tag color={'volcano'}>{item.agente}</Tag>,
+                                            <Tag color={'magenta'}>Escritorio: {item.escritorio}</Tag>,
+                                        ]}
+                                    >
+                                        <Title>Numero: {item.ticketNo}</Title>
+                                    </Card>
+                                </Item>
+
+                            </List>
+                        )}
+                    />
+                </Col>
+                <Col span={12}>
+                    <Divider>Historial</Divider>
+                    <List
+                        dataSource={data.slice(0, 3)}
+                        renderItem={item => (
+                            <Item>
+                                <Card
+                                    title={`Numero de ticket: ${item.ticketNo}`}
+                                >
+                                    <Text type='secondary'>En el escritorio: </Text>
+                                    <Tag color={'magenta'}> {item.ticketNo}</Tag>
+                                    <Text type='secondary'>Agente: </Text>
+                                    <Tag color={'volcano'}> {item.agente}</Tag>
+                                </Card>
+                            </Item>
+                        )}
+                    >
+
+                    </List>
+                </Col>
+            </Row>
+        </>
+    )
 }
