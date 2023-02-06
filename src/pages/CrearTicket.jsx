689a6521d1c9a535cf512c30e7c9b8773881c0d0
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-02-05 21:11:45 -0300

    CreateTicket

--- src/pages/CrearTicket.jsx
+++ src/pages/CrearTicket.jsx
@@ -1,7 +1,52 @@
+import { Button, Col, Row, Typography } from 'antd'
+import { DownloadOutlined } from '@ant-design/icons'
 import React from 'react'
 
 export default function CrearTicket() {
+
+    const { Title, Text } = Typography
+
+    const newTicket = () => {
+        console.log('Nuevo ticket');
+    }
+
     return (
-        <div>CrearTicket</div>
+        <>
+            <Row>
+                <Col
+                    span={14}
+                    offset={6}
+                    style={{ textAlign: 'center' }}
+                >
+                    <Title level={1}>precione el boton para crear un nuevo ticket</Title>
+                    <Button
+                        type='primary'
+                        shape='round'
+                        icon={<DownloadOutlined />}
+                        size='large'
+                        onClick={newTicket}
+                    >
+                        Nuevo ticket
+                    </Button>
+                </Col>
+            </Row>
+            <Row style={{ marginTop: 100 }}>
+                <Col
+                    span={14}
+                    offset={6}
+                    style={{ textAlign: 'center' }}
+                >
+                    <Title level={2} >
+                        su numero:
+                    </Title>
+                    <br />
+                    <Text type='success'
+                    style={{fontSize:55}}
+                    >
+                        55
+                    </Text>
+                </Col>
+            </Row>
+        </>
     )
 }
